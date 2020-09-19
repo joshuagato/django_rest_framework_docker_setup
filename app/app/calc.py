389cd743c121068f527fc1b692aa424209383a84
Joshua Gato <joshuagato37@gmail.com>
2020-09-19 01:26:11 +0000

    linting update

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,7 +1,8 @@
 def add(x, y):
-  """Add two numbers together"""
-  return x + y
+    """Add two numbers together"""
+    return x + y
+
 
 def subtract(x, y):
-  """Subtract x from y and return value"""
-  return y - x
+    """Subtract x from y and return value"""
+    return y - x
